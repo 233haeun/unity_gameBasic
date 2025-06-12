a1e1828d655e3153b4e9c965d61b8bb84259f098
Lee Ha Eun <233haeun@gmail.com>
2025-06-12 17:49:30 +0900

    Add heroCode

--- Assets/Scripts/heroController.cs
+++ Assets/Scripts/heroController.cs
@@ -1,54 +1,142 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
 
 public class heroController : MonoBehaviour
 {
-    Rigidbody2D rigid2D;
-    float jumpForce = 300.0f;
-    float walkForce = 10.0f;
-    float maxWalkSpeed = 2.0f;
-    Animator animator;
-    private float targetFrameRate = 60f;
+    Coroutine runResetCoroutine;
 
+    public GameObject attackCollider;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    private Rigidbody2D rigid2D;
+
+    public float runForce = 30f;
+    public float maxRunSpeed = 60f;
+    public float jumpForce = 900f;
+
+    private bool isRun = false;
+    private bool isJump = false;
+    private bool isAttack = false;
+
     void Start()
     {
-        Time.fixedDeltaTime = 1f / targetFrameRate;
-        this.rigid2D = GetComponent<Rigidbody2D>();
-        this.animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        rigid2D = GetComponent<Rigidbody2D>();
+
+        // ÏãúÏûë Ïãú Step ÏÉÅÌÉúÎ°ú ÏßÑÏûÖ
+        animator.SetBool("isStep", true);
+
+        attackCollider.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //¡°«¡
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            this.rigid2D.AddForce(transform.up * this.jumpForce);
-        }
+        HandleMovement();
+        HandleJump();
+        HandleAttack();
+    }
 
-        //¡¬øÏ¿Ãµø
+    void HandleMovement()
+    {
         int key = 0;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -2; //øﬁ
-        if (Input.GetKey(KeyCode.RightArrow)) key = 2; //ø¿
 
-        float speedx = Mathf.Abs(this.rigid2D.linearVelocity.x);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) key = -2;
+        if (Input.GetKeyDown(KeyCode.RightArrow)) key = 2;
 
-        //speed ¡¶«—
-        if (speedx < this.maxWalkSpeed)
-        {
-            this.rigid2D.AddForce(transform.right * key * this.walkForce);
-        }
+        float speedX = Mathf.Abs(rigid2D.linearVelocity.x);
 
-        //øÚ¡˜¿Ã¥¬ πÊ«‚ø° µ˚∂Û π›¿¸
+        // Î∞©Ìñ• Î∞òÏ†Ñ
         if (key != 0)
         {
             transform.localScale = new Vector3(key, 2, 2);
         }
 
-        //æ÷¥œ∏ﬁ¿Ãº« º”µµ πŸ≤Ÿ±‚
-        this.animator.speed = speedx/2.0f;
+        // Ïù¥Îèô
+        if (key != 0 && speedX < maxRunSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * runForce);
+        }
+
+        // Ïù¥Îèô & Ïï†ÎãàÎ©îÏù¥ÏÖò Ï≤òÎ¶¨
+        if (key != 0 && speedX < maxRunSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * runForce);
+
+            animator.SetBool("isStep", false);
+            animator.SetBool("isRun", true);
+
+
+            if (runResetCoroutine != null)
+                StopCoroutine(runResetCoroutine);
+
+            runResetCoroutine = StartCoroutine(runReset(0.4f));
+
+        }
+        else if (isRun)
+        {
+            animator.SetBool("isRun", false);
+            animator.SetBool("isStep", true);
+        }
+    }
+    IEnumerator runReset(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Í≥µÍ≤© Ï§ëÏù¥Í±∞ÎÇò Ï†êÌîÑ Ï§ëÏù¥Î©¥ StepÏúºÎ°ú ÎèåÏïÑÍ∞ÄÏßÄ ÏïäÏùå
+        if (!isJump && !isAttack)
+        {
+            animator.SetBool("isRun", false);
+            animator.SetBool("isStep", true);
+        }
+
+        runResetCoroutine = null;
+    }
+
+
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            animator.SetBool("isStep", false);
+            animator.SetTrigger("isJump");
+            rigid2D.AddForce(Vector2.up * jumpForce);
+            isJump = true;
+
+            StartCoroutine(jumpReset(0.4f)); // ÏòàÏãú: 0.4Ï¥à ÌõÑ Ï∞©ÏßÄ Í∞ÑÏ£º
+        }
+    }
+    IEnumerator jumpReset(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        isJump = false;
+        animator.SetBool("isStep", true);
+    }
+
+    void HandleAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && !isAttack)
+        {
+            isAttack = true;
+            animator.SetTrigger("isAttack");
+
+            StartCoroutine(ResetAttack(0.5f));
+        }
+    }
+
+    IEnumerator ResetAttack(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        isAttack = false;
+    }
+
+    public void EnableAttackCollider()
+    {
+        attackCollider.SetActive(true);
+    }
+
+    public void DisableAttackCollider()
+    {
+        attackCollider.SetActive(false);
     }
 }
